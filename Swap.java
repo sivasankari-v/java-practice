3d2187d3eebac9fbe102f371d68cef8e3c494369
Sivasankari <sivasankarivenki@gmail.com>
2026-07-04 22:04:45 +0530

    added Swap program

---
+++ Swap.java
@@ -0,0 +1,15 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a=47;
+        int b=24;
+        int temp;
+
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("after swapping");
+        System.out.println("a ="+a);
+        System.out.println("b="+b);
+    }
+    
+}
