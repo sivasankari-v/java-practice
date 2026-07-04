0b7c3ea948fb2d93511a4c59eb6c3498365dd128
Sivasankari <sivasankarivenki@gmail.com>
2026-07-04 18:35:20 +0530

    First java practice upload

---
+++ variable.java
@@ -0,0 +1,12 @@
+public class variable{
+    public static void main(String[] args){
+        int age=20;
+        String name="shiva";
+        
+           System.out.println("name="+name);
+        
+           System.out.println("age="+age);
+          
+    }
+
+}
